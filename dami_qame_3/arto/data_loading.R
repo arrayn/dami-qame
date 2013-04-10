6c75a47ccb93a9b1349224bafb031242f47ad8cc
Arto Nissinen <arto.h.nissinen@gmail.com>
2013-04-10 22:13:53 +0300

    removed dependecy on gsubfn -> tcl/tk -> X11

--- dami_qame_3/arto/data_loading.R
+++ dami_qame_3/arto/data_loading.R
@@ -2,12 +2,15 @@ reformat_data_if_not_done_yet  <- function(mypaths){
   if (file.exists(mypaths$reformatted.data)){return(NULL)}
   cat("STARTED: reformat_data_if_not_done_yet(mypaths)\n")
   pmt <- proc.time()
-  library(gsubfn)
+  # library(gsubfn)
   x <- readLines("../data/courses_sequences_num.txt")
   m <- length(x)
   y <- vector("character")
+  event_strs.list <- regmatches(x, gregexpr("(?<=\\{\\s).*?(?=\\s\\})", x, perl=T))
   for(sid in 1L:m){
-    event_strs <- strapply(x[[sid]], "\\{\\s([^}]*)\\s\\}", c, simplify = c)
+    # event_strs <- strapply(x[[sid]], "\\{\\s([^}]*)\\s\\}", c, simplify = c)
+    # event_strs <- regmatches(x[[sid]], gregexpr("(?<=\\{\\s).*?(?=\\s\\})", x[[sid]], perl=T))[[1]] 
+    event_strs <- event_strs.list[[sid]]
     # print(event_strs)
     for(eid in 1:length(event_strs)){
       temp <- strsplit(event_strs[[eid]], " ")[[1]]
