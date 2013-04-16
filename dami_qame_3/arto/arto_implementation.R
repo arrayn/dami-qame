c86ca0adeb4c75b1e53e90c78f8d879be3774f73
Arto Nissinen <arto.h.nissinen@gmail.com>
2013-04-16 18:31:28 +0300

    Arto's basic implemenation working with 3-sequences.

--- dami_qame_3/arto/arto_implementation.R
+++ dami_qame_3/arto/arto_implementation.R
@@ -1,26 +1,87 @@
-# Fk_1 <- Fk.list[[1]]
-arto.sequence.apriori.gen <- function(Fk_1, debug=FALSE){
-  
+arto.convert.sequences.to.list <- function(s){
   # flatten the idx-list because we are only interested in 1-item elements
-  temp <- LIST(Fk_1, decode=FALSE)
-  # temp <- LIST(arto_implementation.list$s_cspade, decode=FALSE)
+  temp <- LIST(s, decode=TRUE)
   idx.list  <- vector("list", length(temp))
   for(i in 1:length(temp)){idx.list[[i]] <- unlist(temp[[i]])}
+  idx.list
+}
+
+
+# Fk_1 <- Fk.list[[1]]
+arto.sequence.apriori.gen <- function(Fk_1){
+  idx.list <- arto.convert.sequences.to.list(Fk_1)
+
+  # prepare for combining
+  m  <- length(idx.list)
+  k_1 <- length(idx.list[[1]])
+  temp.mat <- matrix("", m, k_1)
+  for(i in 1:m){
+    tr  <- idx.list[[i]]
+    print(tr)
+    temp.mat[i,1:k_1] <- tr
+  }
+  if(k_1==1){
+    lefts <- rights <- matrix(0,m,1)
+  }else{
+    lefts  <- temp.mat[,2:k_1, drop=F]
+    rights <- temp.mat[,1:(k_1-1), drop=F]
+  }
   
-#   if(debug) {
-#     print(system.time( temp <- artoCppAprioriGen(idx.list) ))
-#   }else{ temp <- artoCppAprioriGen(idx.list)}
+  # for matching middles combine lefts and rights
+  out.list <- list()
+  library(gtools)
+  idxs <- permutations(m, 2, repeats.allowed=T)
+  for(i in 1:nrow(idxs)){
+    if(identical(lefts[idxs[i,1]], rights[idxs[i,2]])){
+      tempvec <- c(temp.mat[idxs[i,1],1] , temp.mat[idxs[i,2],])
+      out.list <- append(out.list, list(tempvec))
+    }
+  }
   
-  # inspect(as(temp, "sequences"))
-  # str(arto_implementation.list$s_cspade)
-  
-  out.list <- temp$candidates;
-  count.generated <- temp$generated;
-  ret <- encode(out.list, itemLabels=itemLabels(Fk_1), itemMatrix=TRUE)
-  ret <- new("itemsets", items=ret)
+  ret <- as(out.list, "sequences") 
+  count.generated <- nrow(idxs);
   # returning:
   list(Ck=ret, counts=c(count.generated, length(ret)))
 }
+
+
+
+
+arto.sequence.support <- function(Ck, data.str){
+  cand.list <- arto.convert.sequences.to.list(Ck)
+  nc        <- length(cand.list)
+  supports  <- rep(0L, nc) # returned
+  data.list <- arto.convert.sequences.to.list(data.str)
+  seq.ids   <- as(data.str, "data.frame")[,1] 
+  m <- length(data.str)
+
+  begseq=1; endseq=1; current.seqid=-1; # begin, end of index of sequence
+  for(h in 1:m){
+    if((h %% 1000) == 0) cat(h,"")
+    if(h == endseq){ # new sequence started
+      begseq=h; endseq=h+1;
+      current.seqid <- seq.ids[h]
+      while(endseq <= m && seq.ids[endseq] == current.seqid){ endseq <- endseq+1;}
+      # cat("new seq started: ", current.seqid, begseq, endseq, "\n")
+      for(i in 1:nc){
+        candi <- cand.list[[i]]
+        n <- length(candi)
+        j <- 1 # which event of the candidate are we trying to match
+        for(hh in begseq:(endseq-1)){
+          if(candi[j] %in% data.list[[hh]]){
+            j <- j+1 # move on to matching next event
+            if(j>n) break;
+          }
+        }
+        if(j>n) supports[i] <- supports[i]+1
+      }
+    } 
+  }
+  
+  supports/length(unique(seq.ids))
+}
+
+
 
 
 # data.str <- courses.str
@@ -28,27 +89,29 @@ arto.sequence.apriori <- function(data.str, parameter){
   Fk.list <- list()
   counts.list <- list()
   k <- 1 # line 1
+
   # line 2: Find all frequent 1-subsequences
-  paratemp <- parameter; paratemp$maxwin <- 1 ; paratemp$mingap <- 1
+  paratemp <- parameter; paratemp$maxlen <- 1 ; paratemp$mingap <- 1
   temp <- cspade(data.str, paratemp)
   if(length(temp)==0) stop("no frequent 1-itemsets")
   Fk.list[[k]] <- temp
   counts.list[[k]] <- c(NA,NA,length(Fk.list[[k]]))
   cont <- TRUE # line 3: repeat
-#   while(cont){ 
-#     k <- k+1   # line 4
-#     ret <- arto.sequence.apriori.gen(Fk.list[[k-1]], debug) # line 5: generate candidate itemsets
-#     Ck <-ret$Ck
-#     counts <- ret$counts
-#     if(length(Ck)==0) cont <- FALSE
-# #     #lines 6-11: calculate supports for candidates
-# #     supports <- support(Ck, data.str)
-# #     quality(Ck) <- data.frame(support=supports)    
-# #     Fk.list[k] <- Ck[supports>=minsup,] # line 12
-# #     counts.list[[k]] <- c(counts, length(Fk.list[[k]]))
-#     # line 13: until
-#     if (k >= ncol(data.str) || k>=parameter$maxlen || length(Fk.list[[k]])==0 ) cont <- FALSE   
-#   }
+  while(cont){ 
+    k <- k+1   # line 4
+    ret <- arto.sequence.apriori.gen(Fk.list[[k-1]]) # line 5: generate candidate itemsets
+    Ck <-ret$Ck
+    counts <- ret$counts
+    if(length(Ck)==0) cont <- FALSE
+    #lines 6-11: calculate supports for candidates
+    supports <- arto.sequence.support(Ck, data.str)
+    quality(Ck) <- data.frame(support=supports)    
+    Fk.list[k] <- Ck[supports >= parameter$support,] # line 12
+    # Fk.list[[k]] <- Ck # TEMP HACK
+    counts.list[[k]] <- c(counts, length(Fk.list[[k]]))
+    # line 13: until
+    if (k >= ncol(data.str) || k>=parameter$maxlen || length(Fk.list[[k]])==0 ) cont <- FALSE   
+  }
   # line 14, return the union of Fks
   ret <- Fk.list[[1]]
   for(i in 1:length(Fk.list)) {
--- dami_qame_3/arto/arto_implementation_comput.R
+++ dami_qame_3/arto/arto_implementation_comput.R
@@ -1,15 +1,22 @@
 arto_implementation.list <- vector("list")
 
-parameter <- list(support = 0.20, maxsize=1, maxlen=999, mingap=1, maxgap=999, maxwin=999)
-arto_implementation.list$parameter <- parameter
-arto_implementation.list$s_cspade <- cspade(courses.str, parameter = arto_implementation.list$parameter)
-source("../arto/arto_implementation.R")
-temp <- arto.sequence.apriori(courses.str, parameter=arto_implementation.list$parameter) 
-arto_implementation.list$s_arto <- temp[[1]]
-arto_implementation.list$counts <- temp[[2]]  
+fil <- file.path(mypaths$saves.dir, "arto_implementation.save")
+if(file.exists(fil)){
+  load(fil)
+}else{
+  parameter <- list(support = 0.09, maxsize=1, maxlen=999, mingap=1, maxgap=999, maxwin=999)
+  arto_implementation.list$parameter <- parameter
+  arto_implementation.list$s_cspade <- cspade(courses.str, parameter = arto_implementation.list$parameter)
+  source("../arto/arto_implementation.R")
+  temp <- arto.sequence.apriori(courses.str, parameter=arto_implementation.list$parameter) 
+  arto_implementation.list$s_arto <- temp[[1]]
+  arto_implementation.list$counts <- temp[[2]]
+  save(arto_implementation.list, file=fil)
+}
+
 
 arto_implementation.list$inner_out1  <- function(s){
-  inspect(s)
+#   inspect(s)
   print(as(head(sort(s, by="support"), n=500), "data.frame"))
   print(size(s))
 #   summary(s)
@@ -21,4 +28,5 @@ arto_implementation.list$outme1 <- function(){
 
 arto_implementation.list$outme2 <- function(){
   arto_implementation.list$inner_out1(arto_implementation.list$s_arto)
+  setequal(arto_implementation.list$s_arto, arto_implementation.list$s_cspade)
 }
