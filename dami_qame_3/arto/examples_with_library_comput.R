c2084c15e01cdeb1e90e7521d30569e7f97fc6c6
Arto Nissinen <arto.h.nissinen@gmail.com>
2013-04-16 19:11:22 +0300

    established slides 2

--- dami_qame_3/arto/examples_with_library_comput.R
+++ dami_qame_3/arto/examples_with_library_comput.R
@@ -1,21 +1,12 @@
+arto1 <- list()
 
-str(courses.str)
-summary(courses.str)
-s1 <- cspade(courses.str, parameter = list(support = 0.08), control = list(verbose = TRUE))
-s2 <- cspade(courses.str, parameter = list(support = 0.08, maxsize=1), control = list(verbose = TRUE))
-s3 <- cspade(courses.str, parameter = list(support = 0.08, maxsize=1, mingap=1, maxgap=1), control = list(verbose = TRUE))
-s4 <- cspade(courses.str, parameter = list(support = 0.08, maxwin=5), control = list(verbose = TRUE))
-s1
-s2
-s3
-s4
-summary(s2)
-as(s2, "data.frame")
-summary(s3)
-as(s3, "data.frame")
-
+parameter <- list(support = 0.09, maxsize=1, maxlen=999, mingap=1, maxgap=999, maxwin=999)
+s1 <- cspade(courses.str, parameter = parameter)
 s1
 as(head(sort(s1, by="support"), n=500), "data.frame")
-size(s1)
+
+r1 <- ruleInduction(s1, confidence=0.5)
+r1
+
 
 # R CMD Sweave dami_qame_3_slides_1.Rnw && pdflatex dami_qame_3_slides_1.tex
