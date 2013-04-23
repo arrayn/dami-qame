f81b0e42e5e0337f6f2ace0152fa7dd2f50706d3
Arto Nissinen <arto.h.nissinen@gmail.com>
2013-04-23 18:51:30 +0300

    Added lift-like view of correlation

--- dami_qame_4/arto/negatively_correlated_comput.R
+++ dami_qame_4/arto/negatively_correlated_comput.R
@@ -12,32 +12,53 @@ negap2$inner_worker <- function(bread, coffee){
   colnames(supports) <- paste("P(",colnames(data),")",sep="")
   inde <- as.numeric(supports[1]*supports[2])
   tab2 <- cbind(supports, "P(B)*P(C)"=inde)
+  
+  lift <- as.numeric(supports[3]/inde)
+  lift <- paste("\\[\\frac{P(\\{B,C\\})}{P(B)*P(C)}=", round(lift,2),"\\]", sep="")
   # return
-  list(tab1, tab2)
+  list(tab1=tab1, tab2=tab2, lift=lift)
 }
 
-temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,1,1,1,0,0))
-negap2$tab11 <- temp[[1]]
-negap2$tab12 <- temp[[2]]
-
-temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,1,1,1,0))
-negap2$tab21 <- temp[[1]]
-negap2$tab22 <- temp[[2]]
-
-temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,0,1,1,1))
-negap2$tab31 <- temp[[1]]
-negap2$tab32 <- temp[[2]]
+# temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,1,1,1,0,0))
+# negap2$tab11 <- temp[[1]]
+# negap2$tab12 <- temp[[2]]
+# 
+# temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,1,1,1,0))
+# negap2$tab21 <- temp[[1]]
+# negap2$tab22 <- temp[[2]]
+# 
+# temp <- negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,0,1,1,1))
+# negap2$tab31 <- temp[[1]]
+# negap2$tab32 <- temp[[2]]
 
 #
 # outting function-closures
 #
 
-negap2$inner_outme1 <- function(tab1, tab2){
-  n <- nrow(tab1)
-  print(xtable(tab1, digits=0), hline.after=c(-1,0,n-1,n))
-  print(xtable(tab2, digits=2), include.rownames=F)
+negap2$inner_outme1 <- function(l){
+  n <- nrow(l$tab1)
+  print(xtable(l$tab1, digits=0), hline.after=c(-1,0,n-1,n))
+  print(xtable(l$tab2, digits=2), include.rownames=F)
+  cat(l$lift)
 }
 
-negap2$outme1 <- function(){negap2$inner_outme1(negap2$tab11, negap2$tab12)}
-negap2$outme2 <- function(){negap2$inner_outme1(negap2$tab21, negap2$tab22)}
-negap2$outme3 <- function(){negap2$inner_outme1(negap2$tab31, negap2$tab32)}
+# negap2$inner_outme1 <- function(tab1, tab2){
+#   n <- nrow(tab1)
+#   print(xtable(tab1, digits=0), hline.after=c(-1,0,n-1,n))
+#   print(xtable(tab2, digits=2), include.rownames=F)
+# }
+
+negap2$outme1 <- function(){
+  negap2$inner_outme1(negap2$inner_worker(c(1,1,1,1,0,0), c(0,1,1,1,0,0)))
+}
+
+negap2$outme2 <- function(){
+  negap2$inner_outme1(negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,1,1,1,0)))
+}
+
+negap2$outme3 <- function(){
+  negap2$inner_outme1(negap2$inner_worker(c(1,1,1,1,0,0), c(0,0,0,1,1,1)))
+}
+
+# negap2$outme2 <- function(){negap2$inner_outme1(negap2$tab21, negap2$tab22)}
+# negap2$outme3 <- function(){negap2$inner_outme1(negap2$tab31, negap2$tab32)}
